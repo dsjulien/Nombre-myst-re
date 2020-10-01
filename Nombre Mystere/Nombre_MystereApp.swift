9400550ce90c2b4ae6c1c6f19d140f693c4720fc
Julien de Sousa <contact@juliendesousa.fr>
2020-10-01 10:27:48 +0200

    initial commit

---
+++ Nombre Mystere/Nombre_MystereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nombre_MystereApp.swift
+//  Nombre Mystere
+//
+//  Created by Julien de Sousa on 01/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Nombre_MystereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
